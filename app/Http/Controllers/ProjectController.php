432f53e01bf520cec290f06f385b458a39340a91
leventeproksa <proksalevente@gmail.com>
2022-09-15 13:25:33 +0200

    fixed bootstrap

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -15,6 +15,7 @@ class ProjectController extends Controller
     public function index()
     {
         //
+	    return view("Project.index");
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -14,18 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [ProjectController::class, "index"]);
+Route::get('/', [ProjectController::class, "index"])->name("index");
 
 //create
-Route::get('/project/create', [ProjectController::class, "create"]);
-Route::post('/project/create', [ProjectController::class, "store"]);
+Route::get('/project/create', [ProjectController::class, "create"])->name("project.create");
+Route::post('/project/create', [ProjectController::class, "store"])->name("project.store");
 
 //show
-Route::get('projects/{projectId}', [ProjectController::class, "show"]);
+Route::get('projects/{projectId}', [ProjectController::class, "show"])->name("project.show");
 
 //update
-Route::get('/projects/{projectId}/edit', [ProjectController::class, "edit"]);
-Route::put('/projects/{projectId}/edit', [ProjectController::class, "update"]);
+Route::get('/projects/{projectId}/edit', [ProjectController::class, "edit"])->name("project.edit");
+Route::put('/projects/{projectId}/edit', [ProjectController::class, "update"])->name("project.update");
 
 //delete
-Route::delete('/project/{projectId}/delete', [ProjectController::class, "destroy"]);
+Route::delete('/project/{projectId}/delete', [ProjectController::class, "destroy"])->name("project.destroy");
