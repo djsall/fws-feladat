ee8b015b171f67d243b577399dd817886a474974
leventeproksa <proksalevente@gmail.com>
2022-09-27 23:27:22 +0200

    project creation, index

--- resources/views/project/create.blade.php
+++ resources/views/project/create.blade.php
@@ -4,6 +4,11 @@
 		<div class="row mt-5">
 			@csrf
 			<div class="col-4">
+				@if($errors->any())
+					<div class="form-group">
+						{!! implode('', $errors->all('<div>:message</div>')) !!}
+					</div>
+				@endif
 				<div class="form-group mb-2">
 					<label for="name" class="form-label">
 						Projekt neve:
@@ -22,11 +27,14 @@
 						@endforeach
 					</select>
 				</div>
-				<div class="d-flex justify-content-between mt-5">
+				<div class="d-flex justify-content-between mt-5 mb-2">
 					<p class="form-label">Kapcsolattartók</p>
 					<button type="button" id="add-contact" class="btn btn-sm btn-primary">+</button>
 				</div>
 				<div id="contacts"></div>
+				<div class="form-group mb-2 mt-5">
+					<input type="submit" value="Mentés" class="btn btn-success btn-block w-100">
+				</div>
 			</div>
 		</div>
 	</form>
@@ -37,13 +45,35 @@
 		const btn = $("#add-contact");
 
 		function addInput() {
-			let label = $("<label>").addClass("form-label w-100").text(counter + ". Kapcsolattartó");
-			let input = $("<input>", {name: 'contact-' + counter}).addClass("form-control");
 
-			input.appendTo(label);
-			contacts.append(label);
+			let parentElement = $("<div>", {id: 'parent-element-' + counter});
 
-			console.log("asd");
+			let labelLineParent = $("<div>").addClass("d-flex flex-row justify-content-between");
+			let label = $("<p>").addClass("form-label").text(counter + ". Kapcsolattartó:");
+
+			let destroyBtn = $("<button>", {type: 'button'}).addClass("btn btn-danger btn-sm").text("-");
+
+
+			let nameLabel = $("<label>").addClass("form-label w-100").text("Név:");
+			let nameInput = $("<input>", {required: true, type: 'text', name: 'contacts[contact' + counter + '][name]'}).addClass("form-control");
+			let emailLabel = $("<label>").addClass("form-label w-100").text("E-Mail:");
+			let emailInput = $("<input>", {required: true, type: 'email', name: 'contacts[contact' + counter + '][email]'}).addClass("form-control");
+
+			labelLineParent.append(label);
+			labelLineParent.append(destroyBtn);
+
+			nameInput.appendTo(nameLabel);
+			emailInput.appendTo(emailLabel);
+			parentElement.append(labelLineParent);
+			parentElement.append(nameLabel);
+			parentElement.append(emailLabel);
+
+			contacts.append(parentElement);
+
+			destroyBtn.click(function (event) {
+				$(event.target).parent().parent().remove();
+				counter--;
+			});
 
 			counter++;
 
--- resources/views/project/index.blade.php
+++ resources/views/project/index.blade.php
@@ -6,16 +6,17 @@
 				<h1 class="display-5">Még nem található projekt.</h1>
 				<a href="{{route("project.create")}}" class="btn btn-success">Hozz létre egyet</a>
 			@else
-				@foreach(\App\Models\Project::all() as $project)
-					<div class="card">
+				@foreach($projects as $project)
+					<div class="card mb-3">
 						<div class="card-body">
 							<h5 class="card-title">{{$project->name}}</h5>
 							<p class="card-text">{{$project->description}}</p>
 							<p class="card-text">{{$project->getTranslatedStatus()}}</p>
-							<a href="{{route("project.show", ["projectId"=>1])}}" class="btn btn-primary">Go somewhere</a>
+							<a href="{{route("project.show", ["projectId"=>$project->id])}}" class="btn btn-primary">Részletek</a>
 						</div>
 					</div>
 				@endforeach
+				{{ $projects->links('pagination::bootstrap-5') }}
 			@endif
 		</div>
 	</div>
