2cff73cae7c4431ba9004783a9816cf6595fe729
leventeproksa <proksalevente@gmail.com>
2022-11-17 08:29:06 +0100

    ide helper

--- _ide_helper_models.php
+++ _ide_helper_models.php
@@ -41,6 +41,33 @@ namespace App\Models{
 
 namespace App\Models{
 /**
+ * App\Models\Ticket
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $description
+ * @property string $status
+ * @property int $created_by
+ * @property int $project
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Ticket newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Ticket newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Ticket query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Ticket whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Ticket whereCreatedBy($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Ticket whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Ticket whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Ticket whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Ticket whereProject($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Ticket whereStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Ticket whereUpdatedAt($value)
+ */
+	class Ticket extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
  * App\Models\User
  *
  * @property int $id
